53ccdb83452a6b513da138da26644dac2a3b18fd
ivam <ivam@tut.by>
2022-07-27 19:02:44 +0300

    refactor of tariff view

--- src/main/java/com/ivam/utilityBills/controller/TariffController.java
+++ src/main/java/com/ivam/utilityBills/controller/TariffController.java
@@ -18,46 +18,43 @@ import java.util.List;
 public class TariffController {
 
     @Autowired
-    MeterTypeRepository meterTypeRepository;
-
-    @Autowired
     TariffRepository tariffRepository;
 
     @GetMapping("/tariffs")
     public String findAll(Model model) {
         List<Tariff> tariff = tariffRepository.findAll();
         model.addAttribute("tariff", tariff);
-        return "tariffs";
+        return "tariffs/tariffs";
     }
 
 
-//    @GetMapping("/delete-tariff")
-//    public String delete(@RequestParam int id) {
-//        tariffRepository.deleteById(id);
-//        return "redirect:tariffs";
-//    }
-//
-//    @GetMapping("/add-tariff")
-//    public String add() {
-//        return "add-tariff";
-//    }
-//
-//    @PostMapping("/add-tariff")
-//    public String add(@ModelAttribute Tariff tariff) {
-//        tariffRepository.save(tariff);
-//        return "redirect:tariffs";
-//    }
-//
-//    @GetMapping("/update-tariff")
-//    public String update(@RequestParam int id, Model model) {
-//        Tariff tariff = tariffRepository.findById(id).get();
-//        model.addAttribute("tariff", tariff);
-//        return "update-tariff";
-//    }
-//
-//    @PostMapping("/update-tariff")
-//    public String update(@ModelAttribute Tariff tariff) {
-//        tariffRepository.save(tariff);
-//        return "redirect:tariffs";
-//    }
+    @GetMapping("/delete-tariff")
+    public String delete(@RequestParam int id) {
+        tariffRepository.deleteById(id);
+        return "redirect:tariffs";
+    }
+
+    @GetMapping("add-tariff")
+    public String add() {
+        return "tariffs/add-tariff";
+    }
+
+    @PostMapping("/add-tariff")
+    public String add(@ModelAttribute Tariff tariff) {
+        tariffRepository.save(tariff);
+        return "redirect:tariffs";
+    }
+
+    @GetMapping("/update-tariff")
+    public String update(@RequestParam int id, Model model) {
+        Tariff tariff = tariffRepository.findById(id).get();
+        model.addAttribute("tariff", tariff);
+        return "tariffs/update-tariff";
+    }
+
+    @PostMapping("/update-tariff")
+    public String update(@ModelAttribute Tariff tariff) {
+        tariffRepository.save(tariff);
+        return "redirect:tariffs";
+    }
 }
