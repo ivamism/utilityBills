f37f7dc0ae170c84ac3b24b2562aa6ed1875d5b3
Ivan Mochalov <ivam@tut.by>
2022-08-07 21:06:33 +0300

    Fix bug in update-owner.html

--- src/main/java/com/ivam/utilityBills/model/MetersData.java
+++ src/main/java/com/ivam/utilityBills/model/MetersData.java
@@ -1,25 +1,29 @@
-//package com.ivam.utilityBills.model;
-//
-//import org.springframework.format.annotation.DateTimeFormat;
-//
-//import javax.persistence.*;
-//import java.util.Date;
-//
-//@Entity
-//public class MetersData {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//
-//    @Temporal(TemporalType.DATE)
-//    @DateTimeFormat(pattern = "yyyy-MM-dd")
-//    private Date checkDate;
-//
-//
-//
-////    int previousValue;
-////    int currentValue;
-////    int quantity;
-//}
-//
-//
+package com.ivam.utilityBills.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class MetersData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date checkDate;
+
+
+    @ManyToOne
+    @JoinColumn(name = "meter_id")
+    private Meter meter;
+    private int value;
+
+//    Constructors
+    
+
+}
+
+
