f6d0d97ed7db1855e856d81399c544f2f6d87a58
ivam <ivam@tut.by>
2022-08-19 11:52:26 +0300

    create Update for MetersData
    organize logging

--- src/main/java/com/ivam/utilityBills/controller/MeterDataController.java
+++ src/main/java/com/ivam/utilityBills/controller/MeterDataController.java
@@ -2,13 +2,9 @@ package com.ivam.utilityBills.controller;
 
 
 import com.ivam.utilityBills.model.Meter;
-import com.ivam.utilityBills.model.MeterType;
 import com.ivam.utilityBills.model.MetersData;
-import com.ivam.utilityBills.model.Owners;
 import com.ivam.utilityBills.repository.MeterRepository;
-import com.ivam.utilityBills.repository.MeterTypeRepository;
 import com.ivam.utilityBills.repository.MetersDataRepository;
-import com.ivam.utilityBills.repository.OwnersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,11 +23,6 @@ public class MeterDataController {
 
     @Autowired
     MeterRepository meterRepository;
-
-//    @Autowired
-//    MeterTypeRepository meterTypeRepository;
-//    @Autowired
-//    OwnersRepository ownersRepository;
 
 
     @GetMapping("/metersdata")
@@ -62,20 +53,18 @@ public class MeterDataController {
         return "redirect:metersdata";
     }
 
-//    @GetMapping("/update-meter")
-//    public String update(@RequestParam int id, Model model) {
-//        Meter meter = meterRepository.findById(id).get();
-//        List<MeterType> typeList = meterTypeRepository.findAll();
-//        List<Owners> ownersList = ownersRepository.findAll();
-//        model.addAttribute("typelist", typeList);
-//        model.addAttribute("ownerslist", ownersList);
-//        model.addAttribute("meter", meter);
-//        return "meters/update-meter";
-//    }
-//
-//    @PostMapping("/update-meter")
-//    public String update(@ModelAttribute Meter meter) {
-//        meterRepository.save(meter);
-//        return "redirect:meters";
-//    }
+    @GetMapping("/update-metersdata")
+    public String update(@RequestParam int id, Model model) {
+        MetersData meterData = metersDataRepository.findById(id).get();
+        List<Meter> metersList = meterRepository.findAll();
+        model.addAttribute("metersList", metersList);
+        model.addAttribute("metersData", meterData);
+        return "metersdata/update-metersdata";
+    }
+
+    @PostMapping("/update-metersdata")
+    public String update(@ModelAttribute MetersData meterData) {
+        metersDataRepository.save(meterData);
+        return "redirect:metersdata";
+    }
 }
